5c3a17c0b8a20dec65fec817726946ab11e1d638
MDIMACat <romanmdima@gmail.com>
2024-03-05 13:28:26 +0200

    fixed pycode style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,19 +6,21 @@ Reddit API.
 import requests
 from sys import argv
 
+
 def number_of_subscribers(subreddit):
     """
     returns the number of subscribers for a given subreddit
     """
     baseUrl = f"https://www.reddit.com/r/{subreddit}/about.json"
     header = {'User-Agent': "0x16-api_advanced/1.0"}
-    
+
     resp = requests.get(baseUrl, headers=header, allow_redirects=False)
-    
+
     if (not resp.ok):
         return 0
     sub_count = resp.json().get('data').get('subscribers')
     return sub_count
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
