c9f6bfd0185575aabca9b837814d93e2286d948b
MDIMACat <romanmdima@gmail.com>
2024-03-05 13:26:34 +0200

    fixed attribute error in task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,21 +4,21 @@ Define a function that queries the
 Reddit API.
 """
 import requests
+from sys import argv
 
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers for a given subreddit
+    """
+    baseUrl = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {'User-Agent': "0x16-api_advanced/1.0"}
+    
+    resp = requests.get(baseUrl, headers=header, allow_redirects=False)
+    
+    if (not resp.ok):
+        return 0
+    sub_count = resp.json().get('data').get('subscribers')
+    return sub_count
 
 if __name__ == "__main__":
-    
-    def number_of_subscribers(subreddit):
-        """
-        returns the number of subscribers for a given subreddit
-        """
-        baseUrl = f"https://www.reddit.com/r/{subreddit}/about.json"
-        header = {'User-Agent': "0x16-api_advanced/1.0"}
-        
-        resp = requests.get(baseUrl, headers=header, allow_redirects=False)
-        
-        if (not resp.ok):
-            return 0
-        sub_count = resp.json().get('data').get('subscribers')
-        return sub_count
-
+    number_of_subscribers(argv[1])
